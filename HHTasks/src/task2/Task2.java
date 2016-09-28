ba0c942833752b6b5930c0a66b8f2b26df2a180e
A.kovalev <a.kovalev@inforion.ru>
2016-09-28 12:46:54 +0300

    task2 alpha

--- HHTasks/src/task2/Task2.java
+++ HHTasks/src/task2/Task2.java
@@ -1,8 +1,11 @@
 package task2;
 
+import sun.util.resources.cldr.st.CurrencyNames_st;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,23 +14,51 @@ import java.util.Scanner;
  * Created by A.Kovalev on 28.09.2016.+
  */
 public class Task2 {
+	private static StringBuilder _str = new StringBuilder();
+	private static long _cur = 1;
+
 	public static void main(String[] args) {
-		long[] input = getInput();
-		for (long l : input) System.out.println(l);
+		//long[] input = getInput();
+		System.out.println(find("11"));
+		//for (long l : input) System.out.println(l);
 	}
 
-	private static long[] getInput() {
+	private static String[] getInput() {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> input = new LinkedList<>();
+		List<String> input = new ArrayList<>();
 		try {
 			while (true) {
 				String str = in.readLine();
 				if (str.isEmpty()) break;
-				input.add(Integer.parseInt(str));
+				if (!str.matches("\\d+")) throw new IllegalArgumentException("Wrong input!");
+				input.add(str);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return input.stream().mapToLong(l -> l).toArray();
+		return input.toArray(new String[0]);
+	}
+
+	public static int find(String find)
+	{
+		int hashToFind = find.hashCode();
+		// Start string init
+		while (find.length() > _str.length()){
+			_str.append(_cur++);
+		}
+		int subHash;
+		int i = 0;
+		while (true){
+			String sub = _str.substring(i, find.length() + i);
+			subHash = sub.hashCode();
+			if (subHash == hashToFind){
+				if (sub.equals(find)) return i;
+			}
+			// extend
+			i++;
+			while (_str.length() < find.length() + i){
+				_str.append(_cur++);
+			}
+		}
 	}
 }
