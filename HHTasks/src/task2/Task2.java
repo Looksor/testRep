4364ec45b676916950caee61dab94c5b260a8a90
A.kovalev <a.kovalev@inforion.ru>
2016-09-28 17:11:24 +0300

    task2 minor fix

--- HHTasks/src/task2/Task2.java
+++ HHTasks/src/task2/Task2.java
@@ -1,14 +1,10 @@
 package task2;
 
-import sun.util.resources.cldr.st.CurrencyNames_st;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Created by A.Kovalev on 28.09.2016.+
@@ -21,7 +17,7 @@ public class Task2 {
 		String[] result;
 		String[] input = getInput();
 		result = new String[input.length];
-		for (int i = 0; i < input.length; i++){
+		for (int i = 0; i < input.length; i++) {
 			result[i] = find(input[i]);
 		}
 		for (String l : result) System.out.println(l);
@@ -33,36 +29,36 @@ public class Task2 {
 		try {
 			while (true) {
 				String str = in.readLine();
-				if (str.isEmpty()) break;
-				if (!str.matches("\\d+")) throw new IllegalArgumentException("Wrong input!");
+				if (str.isEmpty()) break;   // End of input
+				if (!str.matches("\\d+")) throw new IllegalArgumentException("Wrong input!");   // Not number
 				input.add(str);
 			}
+			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+			throw new RuntimeException("Input failed!");
 		}
 		return input.toArray(new String[0]);
 	}
 
-	private static String find(String find)
-	{
+	private static String find(String find) {
 		int hashToFind = find.hashCode();
 		// Start string init
-		while (find.length() > _str.length()){
-			_str.append(_cur++);
-		}
-		int subHash;
+		while (find.length() > _str.length()) _str.append(_cur++);
+		// Loop for search
+		int subHash;    // NOTE - на самом деле я думал переписать вычисление хеша, но оказалось, что оно и так неплохо работает
 		int i = 0;
-		while (true){
+		while (true) {
+			// sub hash and template hash
 			String sub = _str.substring(i, find.length() + i);
 			subHash = sub.hashCode();
-			if (subHash == hashToFind){
-				if (sub.equals(find)) return (i + 1) + "";
-			}
-			// extend
+
+			// find some -> equals check -> result
+			if (subHash == hashToFind && sub.equals(find)) return (i + 1) + "";
+
+			// extend str
 			i++;
-			while (_str.length() < find.length() + i){
-				_str.append(_cur++);
-			}
+			while (_str.length() < find.length() + i) _str.append(_cur++);
 		}
 	}
 }
