f08993fd14cc3fb132f4f183af9c92608904aa82
A.kovalev <a.kovalev@inforion.ru>
2016-09-28 17:23:05 +0300

    task1 template

--- HHTasks/src/task1/Task1.java
+++ HHTasks/src/task1/Task1.java
@@ -9,20 +9,36 @@ import java.io.InputStreamReader;
  */
 public class Task1 {
 	public static void main(String[] args) {
-		int[][][] input = getInputData();
-		print(input);
+		//int[][][] input = getInputData();
+		//print(input);
+		int[][][] test1 = {{{5, 3, 4, 5},
+							{6, 2, 1, 4},
+							{3, 1, 1, 4},
+							{8, 5, 4, 3}}};
+		int[] result = calculate(test1);
 	}
 
-	private static void print(int[][][] input) {
-		for (int[][] matrix : input){
-			for (int[] row: matrix){
-				for (int el: row){
-					System.out.print(el + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
+	private static int[] calculate(int[][][] input) {
+		int[] result = new int[input.length];
+		for (int n = 0; n < input.length; n++){
+			int[][] matrix = input[n];
+			int[][] tempColMatrix = calcCol(matrix);
+//			int[][] tempRowMatrix = calcRow(matrix);
+//			result[n] = getResult(tempColMatrix, tempRowMatrix);
 		}
+		return result;
+	}
+
+	private static int[][] calcCol(int[][] matrix) {
+		return null;
+	}
+
+	private static int[][] calcRow(int[][] matrix) {
+		return null;
+	}
+
+	private static int getResult(int[][] tempColMatrix, int[][] tempRowMatrix) {
+		return 0;
 	}
 
 	private static int[][][] getInputData() {
@@ -62,5 +78,15 @@ public class Task1 {
 		return result;
 	}
 
-
+	private static void print(int[][][] input) {
+		for (int[][] matrix : input){
+			for (int[] row: matrix){
+				for (int el: row){
+					System.out.print(el + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
 }
