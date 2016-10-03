b4d795b18da2904620a080a736924d20a3fbd2de
A.kovalev <a.kovalev@inforion.ru>
2016-10-03 12:01:55 +0300

    task1 release

--- HHTasks/src/task1/Task1.java
+++ HHTasks/src/task1/Task1.java
@@ -5,72 +5,140 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
- * Created by A.Kovalev on 27.09.2016.+
+ * Created by A.Kovalev on 27.09.2016.
+ *
+ * Реализовал первый алгоритм который пришёл в голову
+ * Суть - "заполнить низины" отдельно по строкам и столбцам, а затем взять наименьшее из 2 значений
+ * алгоритм не самый оптимальный, но на разработку другого не нашлось времени :(
+ *
+ * Известные баги - неточость в матрицах со сложными низинами, например:
+ *
+ * 4 4 4        4 4 4           4 4 4
+ * 4 1 4        4 4 4           4 1 4
+ * 1 1 4    =>  1 1 4   вместо  1 1 4
+ * 4 1 4        4 4 4           4 1 4
+ * 4 4 4        4 4 4           4 4 4
+ *
+ * PS возможно было бы проще считать матрицу от краёв к центру
  */
 public class Task1 {
 	public static void main(String[] args) {
-		//int[][][] input = getInputData();
-		//print(input);
-		int[][][] test1 = {{{5, 3, 4, 5},
-				{6, 2, 1, 4},
-				{3, 1, 1, 4},
-				{8, 5, 4, 3}}};
-		int[] result = calculate(test1);
+		int[][][] input = getInputData();
+		int[] result = calculate(input);
+		for (int i : result) System.out.println(i);
 	}
 
 	private static int[] calculate(int[][][] input) {
 		int[] result = new int[input.length];
 		for (int n = 0; n < input.length; n++) {
 			int[][] matrix = input[n];
-//			int[][] tempColMatrix = calcCol(matrix);
-			int[][] tempRowMatrix = calcRow(matrix);
-			print(tempRowMatrix);
-//			result[n] = getResult(tempColMatrix, tempRowMatrix);
+			int width = matrix.length;
+			int heigh = matrix[0].length;
+
+			// 2 матрицы для расчета по колонкам и столбцам
+			int[][] rowMatrix = new int[width][heigh];
+			int[][] colMatrix = new int[width][heigh];
+			for (int i = 0; i < width; i++) {
+				rowMatrix[i] = matrix[i].clone();
+				colMatrix[i] = matrix[i].clone();
+			}
+			// calculate row
+			rowMatrix = calculateRow(rowMatrix);
+
+			// calculate col
+			colMatrix = calculateCol(colMatrix);
+
+			// calculate water level
+			result[n] = getResult(matrix, colMatrix, rowMatrix);
 		}
 		return result;
 	}
 
-	private static int[][] calcCol(int[][] matrix) {
-		return null;
-	}
-
-	private static int[][] calcRow(int[][] matrix) {
-		int width = matrix.length;
-		int heigh = matrix[0].length;
-		int result = 0;
+	private static int[][] calculateRow(int [][] rowMatrix){
+		int width = rowMatrix.length;
+		int heigh = rowMatrix[0].length;
 		for (int i = 1; i < width - 1; i++) {
-			int prev = matrix[i][0];
-			int cur = 0;
-			int max_index = 0;
-			int max_value = matrix[i][0];
-			int water_count = 0;
+			int prev = rowMatrix[i][0];
+			int cur;
+			int maxIndex = 0;
+			int maxValue = rowMatrix[i][0];
+
 			for (int j = 1; j < heigh; j++) {
-				cur = matrix[i][j];
-//				System.out.println("Cur = " + cur + " prev = " + prev);
-				if (cur > max_value) {
-					for (int k = j - 1; k > max_index; k--) {
-						matrix[i][k] = max_value;
+				cur = rowMatrix[i][j];
+				if (cur > maxValue) {
+					// Заполнение, так как нашлось более высокое значение
+					for (int k = j - 1; k > maxIndex; k--) {   // От текущего значения до прошлой вершины
+						rowMatrix[i][k] = maxValue;
 					}
-					max_value = cur;
-					max_index = j;
+					maxValue = cur;
+					maxIndex = j;
 				} else {
-					if (cur > prev){    // Fill
-//						System.out.println("!! Cur = " + cur + " > prev = " + prev);
-						for (int k = j - 1; k > max_index; k--) {
-							matrix[i][k] = cur;
+					// Если началось возрастание (образовалась низина)
+					if (cur > prev) {
+						// Заполнение, так как образовалась низина
+						for (int k = j - 1; k > maxIndex; k--) {   // От текущего значения до прошлой вершины
+							if (rowMatrix[i][k] > cur) break;      // Если левый склон уже выше правой точки
+							rowMatrix[i][k] = cur;
 						}
 					}
 				}
 				prev = cur;
 			}
 		}
-		return matrix;
+		return rowMatrix;
 	}
 
-	private static int getResult(int[][] tempColMatrix, int[][] tempRowMatrix) {
-		return 0;
+	private static int[][] calculateCol(int [][] colMatrix){
+		int width = colMatrix.length;
+		int heigh = colMatrix[0].length;
+		for (int i = 1; i < heigh - 1; i++) {
+			int prev = colMatrix[0][i];
+			int cur;
+			int maxIndex = 0;
+			int maxValue = colMatrix[0][i];
+
+			for (int j = 1; j < width; j++) {
+				cur = colMatrix[j][i];
+				if (cur > maxValue) {
+					// Заполнение, так как нашлось более высокое значение
+					for (int k = j - 1; k > maxIndex; k--) {   // От текущего значения до прошлой вершины
+						colMatrix[k][i] = maxValue;
+					}
+					maxValue = cur;
+					maxIndex = j;
+				} else {
+					// Если началось возрастание (образовалась низина)
+					if (cur > prev) {
+						// Заполнение, так как образовалась низина
+						for (int k = j - 1; k > maxIndex; k--) {   // От текущего значения до прошлой вершины
+							if (colMatrix[k][i] > cur) break;      // Если верхний склон уже выше нижней точки
+							colMatrix[k][i] = cur;
+						}
+					}
+				}
+				prev = cur;
+			}
+		}
+		return colMatrix;
+	}
+
+	private static int getResult(int[][] matrix, int[][] colMatrix, int[][] rowMatrix) {
+		int res = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] != colMatrix[i][j] || matrix[i][j] != rowMatrix[i][j]) {   // Если есть изменения
+					if (colMatrix[i][j] < rowMatrix[i][j]) {    // берем меньшее
+						res += colMatrix[i][j] - matrix[i][j];
+					} else {
+						res += rowMatrix[i][j] - matrix[i][j];
+					}
+				}
+			}
+		}
+		return res;
 	}
 
 	private static int[][][] getInputData() {
